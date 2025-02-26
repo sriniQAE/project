acf81c2393e2fb31c30517cfa797a1a25462e8b8
sriniQAE <srinisrini@gmail.com>
2025-02-26 23:53:35 +0530

    FirstTimePush

---
+++ src/test/java/com/omr/Sample.java
@@ -0,0 +1,17 @@
+package com.omr;
+
+public class Sample {
+
+	public void hi() {
+		System.out.println("Hai");
+	}
+
+	public void help() {
+		System.out.println("Help");
+	}
+
+	public void hello() {
+		System.out.println("Hello");
+	}
+
+}
